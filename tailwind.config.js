c1d59f9611200483e49bb4b8d5ee197317f0fec9
ulvi <atayevulvi5@gmail.com>
2023-02-21 17:42:43 +0400

    class deyisib

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,27 @@ module.exports = {
     extend: {
       backgroundImage:{
         "arxa-fon": "url('../src/assets/images/asagi-arxa-fon.jpg')"
+      },
+      keyframes: {
+        bir: {
+          '0%, 50%': { transform: 'translatey(14px)' },
+          '100%': { transform: 'translatey(-50px)' },
+        },
+      },
+      animation: {
+        bir: 'bir 1.5s infinite',
+      },
+
+      keyframes:  {
+        iki: {
+          '0%, 50%': { transform: 'translatey(-14px)' },
+          '100%': { transform: 'translatey(-50px)' },
+        },
+      },
+      animation: {
+        iki: 'iki 1.5s  infinite',
       }
-    },
+    }
   },
   plugins: [],
 }
